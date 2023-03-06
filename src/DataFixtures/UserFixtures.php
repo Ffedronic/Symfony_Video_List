6bb1f58705142356484cc45b7003355489aa905d
felix fedronic <fedronicfelix@gmail.com>
2023-03-06 20:50:57 +0100

    userfixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    public $userPasswordHasherInterface;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+        $this->userPasswordHasherInterface = $userPasswordHasherInterface;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach ($this->UserData() as [$name, $last_name, $email, $password, $api_key, $roles]) {
+            $user = new User();
+
+            $user->setName($name);
+            $user->setLastName($last_name);
+            $user->setEmail($email);
+            $user->setPassword($this->userPasswordHasherInterface->hashPassword($user, $password));
+            $user->setVimeoApiKey($api_key);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
+    }
+
+    private function UserData()
+    {
+        return [
+
+            ['felix', 'fedronic', 'felix.fedronic@orange.fr', 'password', 'hjd8dehdh', ['ROLE_ADMIN']],
+            ['inès', 'fedronic', 'ines.fedronic@orange.fr', 'password', null, ['ROLE_ADMIN']],
+            ['alain', 'fedronic', 'alain.fedronic@orange.fr', 'password', null, ['ROLE_USER']],
+        ];
     }
 }
