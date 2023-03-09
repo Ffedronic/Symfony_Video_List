d597ef5edcb48518f8223e74d29bba268fb65bab
felix fedronic <fedronicfelix@gmail.com>
2023-03-09 20:20:32 +0100

    comment fixtures

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 class Comment
 {
-    private ?string $created_at2 = null;
+    private ?DateTime $created_at2 = null;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
